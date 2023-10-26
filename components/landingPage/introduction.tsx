659b3f9ba245a085af1f3b02d1e7ca2194fd38a0
arhamabeer <arhamabeerahmed@gmail.com>
2023-10-26 17:35:50 +0500

    feat: global css updated with global container's css

---
+++ components/landingPage/introduction.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Introduction() {
+  return (
+    <div>
+      <h1 className="text-6xl font-bold">
+        <span className="text-blue-700">Develop your apps</span>
+        <br /> more faster than ever
+        <br /> with <span className="text-blue-700">Next Pallette!</span>
+      </h1>
+    </div>
+  );
+}
+
+export default Introduction;
