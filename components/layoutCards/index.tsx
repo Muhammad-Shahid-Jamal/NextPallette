2a4cdda91860a104da6ac138b4dfd772f91e8b80
arhamabeer <arhamabeerahmed@gmail.com>
2023-10-26 18:37:10 +0500

    fix: svg errors fixed

--- components/layoutCards/index.tsx
+++ components/layoutCards/index.tsx
@@ -33,9 +33,9 @@ function LayoutCards(props: DataProps) {
         >
           <path
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M1 5h12m0 0L9 1m4 4L9 9"
           />
         </svg>
